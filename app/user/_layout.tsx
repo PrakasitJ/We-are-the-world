8054ac94fffa97a2a52197f12bc9876812c38464
HomieZ09 <phumsoen123@gmail.com>
2025-02-06 17:01:51 +0700

    Feat: Add user navigation and home screen layout

---
+++ app/user/_layout.tsx
@@ -0,0 +1,47 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+// import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  // const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        // tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: Colors["light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          android: {
+            // Use a custom background color on Android to avoid the default one
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />      
+    </Tabs>
+  );
+}
