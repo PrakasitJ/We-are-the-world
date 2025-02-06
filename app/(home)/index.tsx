be1bc8cb106809e4f9eb418967f71012e2f06c9d
Malaming <minggggggg@gmail.com>
2025-02-06 19:53:44 +0700

    Feat: Update home screen layout with personalized greeting and new content structure

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Platform } from "react-native";
+import { StyleSheet, Platform, View, Text } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import { ThemedText } from "@/components/ThemedText";
@@ -9,44 +9,13 @@ export default function HomeScreen() {
   const insets = useSafeAreaInsets();
   return (
     <ThemedView style={{ paddingTop: insets.top }}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!Status Bar Height: {insets.top}px</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <View className="flex flex-column justify-end pb-6 pl-6 bg-[#253D2C] h-[250]"><Text className = "text-4xl font-extrabold text-white " >Hello, Phunyisa</Text></View>
+      <View className="flex flex-column">
+       <View></View>
+       <View><Text>ส่งที่ฉัน</Text><View></View></View>
+       <View></View>
+       <View></View>
+      </View>
     </ThemedView>
   );
 }
