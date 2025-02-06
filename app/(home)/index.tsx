a5bcfec08a361b3078787407076017fccba9df65
HomieZ09 <phumsoen123@gmail.com>
2025-02-06 17:42:26 +0700

    Feat: Refactor home layout and navigation structure

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,21 +1,12 @@
-import { Image, StyleSheet, Platform, Text } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
+    <ThemedView style={styles.paddingHeader}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
@@ -54,7 +45,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
-    </ParallaxScrollView>
+    </ThemedView>
   );
 }
 
@@ -74,5 +65,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: "absolute",
+  },
+  paddingHeader: {
+    paddingTop: 40,
+    paddingHorizontal: 16,
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,6 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { Slot } from "expo-router";
 import "./tailwind.css";
 import "react-native-reanimated";
 
@@ -37,12 +36,13 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        {/* <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" /> */}
-        <Slot />
+        <Stack>
+          <Stack.Screen name="(home)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+        {/* <Slot /> */}
       </ThemeProvider>
     </GestureHandlerRootView>
   );
