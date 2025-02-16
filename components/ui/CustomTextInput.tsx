feadd632731ad9e364c52a200b824d53244d26a4
Kriwn <nonisgod@gmail.com>
2025-02-16 17:17:55 +0700

    add width textinput

--- components/ui/CustomTextInput.tsx
+++ components/ui/CustomTextInput.tsx
@@ -12,13 +12,13 @@ export default function CustomTextInput({ title ,placeholder, onChange }: { titl
 	return (
 		<View className="flex-row justify-between w-full h-[50px] bg-gray-200 rounded-[5px] mb-[10px] px-3">
 			<TextInput
-				className="text-[16px] text-black"
+				className="text-[16px] text-black w-[90%]"
 				placeholder={placeholder}
 				onChangeText={handleChange}
 				secureTextEntry={title === "Password" ? !showPassword : false}
 			/>
 			{title === "Password" && (
-				<TouchableOpacity className="justify-center place-self-end" onPress={() => setShowPassword(!showPassword)}>
+				<TouchableOpacity className="justify-center" onPress={() => setShowPassword(!showPassword)}>
 					<Feather name={showPassword ? "eye" : "eye-off"} size={10} color="black" />
 				</TouchableOpacity>
 			)}
