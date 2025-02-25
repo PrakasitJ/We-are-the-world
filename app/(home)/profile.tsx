60be9dec3e0cf05f30bbec65accae5602118abcb
Malaming <minggggggg@gmail.com>
2025-02-25 23:50:18 +0700

    Fix(Profile): clean up code formatting and improve text styles in profile component

--- app/(home)/profile.tsx
+++ app/(home)/profile.tsx
@@ -4,7 +4,7 @@ import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
 
 export default function profile() {
     const [showEditNames, setShowEditName] = useState<boolean>(false);
-    const [name, setName] = useState<string>("ชื่อผู้ใช้"); 
+    const [name, setName] = useState<string>("ชื่อผู้ใช้");
 
     return (
         <View className="flex-1">
@@ -21,7 +21,7 @@ export default function profile() {
                     </TouchableOpacity>
 
                 </View>
-                <Text className="text-xl text-black font-medium mt-3 justify-center items-center">เบอร์โทรศัพท์</Text>
+                <Text className="text-xl text-black font-medium mt-3 justify-center items-center ">เบอร์โทรศัพท์</Text>
             </View>
             <View className="flex-1 justify-end items-center pb-[100px]">
                 <TouchableOpacity className="">
@@ -34,31 +34,31 @@ export default function profile() {
 }
 
 
-const ModalProfile = ({ setShowEditName, setName}: { setShowEditName: React.Dispatch<React.SetStateAction<boolean>>, setName: React.Dispatch<React.SetStateAction<string>>  }) => {
+const ModalProfile = ({ setShowEditName, setName }: { setShowEditName: React.Dispatch<React.SetStateAction<boolean>>, setName: React.Dispatch<React.SetStateAction<string>> }) => {
     const [searchValue, setSearchValue] = useState<string>("");
 
     const handleConfirm = () => {
         setName(searchValue);
         setShowEditName(false);
-      };
-      
+    };
+
     return (
         <View className="absolute items-center justify-center h-full w-full bg-black/50 ">
             <View className="flex  p-7 justify-center w-[350px] h-[250px] bg-gray-100 rounded-lg shadow-lg">
-                <Text className="font-medium mb-4 text-xl ">แก้ไขชื่อผู้ใช้</Text>
+                <Text className="font-medium mb-4 text-xl font-regular">แก้ไขชื่อผู้ใช้</Text>
                 <TextInput
-                        editable
-                        value={searchValue}
-                        placeholder="ชื่อผู้ใช้"
-                        placeholderTextColor= "#354138 opacity-50"
-                        className="w-full h-[40px] rounded-[10px] bg-[#D9D9D9]  pl-[20px]"
-                        onChangeText={setSearchValue}
-                      />
+                    editable
+                    value={searchValue}
+                    placeholder="ชื่อผู้ใช้"
+                    placeholderTextColor="#354138 opacity-50"
+                    className="w-full h-[40px] rounded-[10px] bg-[#D9D9D9]  pl-[20px]"
+                    onChangeText={setSearchValue}
+                />
                 <TouchableOpacity onPress={() => handleConfirm()} className="mt-4" >
-                    <Text className="text-base text-white bg-[#68Ba7f] py-[15px] text-center rounded-full font-semibol">ยืนยัน</Text>
+                    <Text className="text-base text-white bg-[#68Ba7f] py-[15px] text-center rounded-full font-semibol font-regular">ยืนยัน</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => setShowEditName(false)} className="mt-4">
-                    <Text className="text-base text-[#68Ba7f]  py-[5px] text-center rounded-full font-semibol shadow-slate-500">ยกเลิก</Text>
+                    <Text className="text-base text-[#68Ba7f]  py-[5px] text-center rounded-full font-semibol shadow-slate-500 font-regular">ยกเลิก</Text>
                 </TouchableOpacity>
             </View>
         </View>
