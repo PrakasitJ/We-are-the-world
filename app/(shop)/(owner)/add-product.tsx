79d4159e516b7186b793a8211215268f0cd1fd4e
HomieZ09 <phumsoen123@gmail.com>
2025-03-29 00:11:04 +0700

    style(shop): enhance AddProduct component with improved layout, styling, and icon integration

--- app/(shop)/(owner)/add-product.tsx
+++ app/(shop)/(owner)/add-product.tsx
@@ -4,6 +4,7 @@ import useShops from '@/app/provider/shops';
 import useProductForm from '@/app/provider/productForm';
 import { useRouter } from 'expo-router';
 import { Picker } from '@react-native-picker/picker';
+import { Ionicons } from '@expo/vector-icons';
 
 const AddProduct = () => {
     const router = useRouter();
@@ -48,72 +49,87 @@ const AddProduct = () => {
     };
 
     return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.title}>เพิ่มผลิตภัณฑ์</Text>
-            <View style={styles.inputContainer}>
-                <Text style={styles.label}>ชื่อผลิตภัณฑ์</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="ชื่อผลิตภัณฑ์"
-                    value={product?.name}
-                    onChangeText={(text) => setProduct({ ...product, name: text })}
-                />
-                <Text style={styles.label}>ราคา</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="ราคา"
-                    value={product?.price.toString()}
-                    onChangeText={(text) => setProduct({ ...product, price: Number(text) })}
-                    keyboardType="numeric"
-                />
-                <Text style={styles.label}>คำอธิบาย</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="คำอธิบาย"
-                    value={product?.description}
-                    onChangeText={(text) => setProduct({ ...product, description: text })}
-                />
-                <Text style={styles.label}>URL รูปภาพผลิตภัณฑ์</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="URL รูปภาพผลิตภัณฑ์"
-                    value={product?.image_url}
-                    onChangeText={(text) => setProduct({ ...product, image_url: text })}
-                />
-                <Text style={styles.label}>จำนวน</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="จำนวน"
-                    value={product?.amount.toString()}
-                    onChangeText={(text) => setProduct({ ...product, amount: Number(text) })}
-                    keyboardType="numeric"
-                />
-                <View style={styles.categoryContainer}>
-                    <Text style={styles.label}>หมวดหมู่</Text>
-                    <TouchableOpacity style={styles.categoryButton} onPress={() => router.push('/(shop)/(owner)/add-category')}>
-                    <Text style={styles.categoryButtonText}>เพิ่มหมวดหมู่</Text>
+        <>
+            <View className="bg-[#253D2C] w-full h-[60px] flex-row justify-between items-center px-5" />
+            <View className="bg-[#253D2C] w-full h-[60px] flex-row justify-between items-center px-5">
+                <TouchableOpacity onPress={() => router.back()}>
+                    <Ionicons name="chevron-back-outline" size={24} color="white" />
+                </TouchableOpacity>
+                <Text className="font-regular text-white text-center items-center text-xl">เพิ่มผลิตภัณฑ์</Text>
+                <TouchableOpacity className="opacity-0">
+                    <Ionicons name="add-outline" size={24} color="white" />
+                </TouchableOpacity>
+            </View>
+            <ScrollView className="bg-[#2A312C] w-full h-full">
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>ชื่อผลิตภัณฑ์</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="ชื่อผลิตภัณฑ์"
+                        value={product?.name}
+                        onChangeText={(text) => setProduct({ ...product, name: text })}
+                    />
+                    <Text style={styles.label}>ราคา</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="ราคา"
+                        value={product?.price.toString()}
+                        onChangeText={(text) => setProduct({ ...product, price: Number(text) })}
+                        keyboardType="numeric"
+                    />
+                    <Text style={styles.label}>คำอธิบาย</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="คำอธิบาย"
+                        value={product?.description}
+                        onChangeText={(text) => setProduct({ ...product, description: text })}
+                    />
+                    <Text style={styles.label}>URL รูปภาพผลิตภัณฑ์</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="URL รูปภาพผลิตภัณฑ์"
+                        value={product?.image_url}
+                        onChangeText={(text) => setProduct({ ...product, image_url: text })}
+                    />
+                    <Text style={styles.label}>จำนวน</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="จำนวน"
+                        value={product?.amount.toString()}
+                        onChangeText={(text) => setProduct({ ...product, amount: Number(text) })}
+                        keyboardType="numeric"
+                    />
+                    <View style={styles.categoryContainer}>
+                        <Text style={styles.label}>หมวดหมู่</Text>
+                        <TouchableOpacity style={styles.categoryButton} onPress={() => router.push('/(shop)/(owner)/add-category')}>
+                            <Text style={styles.categoryButtonText}>เพิ่มหมวดหมู่</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Picker
+                        selectedValue={product?.category_id}
+                        onValueChange={(value) => setProduct({ ...product, category_id: value })}
+                        style={styles.picker}
+                        className='font-regular'
+                        dropdownIconColor="#4CAF50"
+                        dropdownIconRippleColor="#4CAF50"
+                        itemStyle={styles.pickerItem}
+                    >
+                        <Picker.Item label="เลือกหมวดหมู่" value={-1} />
+                        {categories.map((category) => (
+                            <Picker.Item style={styles.pickerItem} key={category.id} label={category.category_name} value={category.id} />
+                        ))}
+                    </Picker>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+                        <Text style={styles.backButtonText}>กลับ</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.confirmButton} onPress={handleConfirm}>
+                        <Text style={styles.buttonText}>ยืนยัน</Text>
                     </TouchableOpacity>
                 </View>
-                <Picker
-                    selectedValue={product?.category_id}
-                    onValueChange={(value) => setProduct({ ...product, category_id: value })}
-                    style={styles.picker}
-                >
-                    <Picker.Item label="เลือกหมวดหมู่" value={-1} />
-                    {categories.map((category) => (
-                        <Picker.Item key={category.id} label={category.category_name} value={category.id} />
-                    ))}
-                </Picker>
-            </View>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
-                    <Text style={styles.backButtonText}>กลับ</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.confirmButton} onPress={handleConfirm}>
-                    <Text style={styles.buttonText}>ยืนยัน</Text>
-                </TouchableOpacity>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </>
     );
 };
 
@@ -133,12 +149,15 @@ const styles = StyleSheet.create({
         textAlign: 'center',
     },
     inputContainer: {
+        paddingTop: 25,
         marginBottom: 30,
+        paddingHorizontal: 20,
     },
     input: {
         backgroundColor: '#ffffff',
         borderWidth: 1,
         borderColor: '#E0E0E0',
+        fontFamily: 'notoSansThai-Regular',
         borderRadius: 12,
         padding: 15,
         marginBottom: 15,
@@ -156,16 +175,22 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         marginBottom: 5,
     },
+    pickerItem: {
+        fontFamily: 'notoSansThai-Regular',
+        color: '#000000',
+    },
     label: {
         fontSize: 16,
-        fontWeight: '600',
-        color: '#4CAF50', // Match Register component label color
+        fontWeight: '500',
+        fontFamily: 'notoSansThai-Regular',
+        color: '#FFFFFF', // Match Register component label color
         marginBottom: 5,
     },
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginBottom: 30,
+        paddingHorizontal: 20
     },
     backButton: {
         width: 100,
@@ -201,6 +226,8 @@ const styles = StyleSheet.create({
     },
     categoryButtonText: {
         color: 'white',
+        padding: 3,
+        fontFamily: 'notoSansThai-Regular',
         fontSize: 14,
         fontWeight: '600',
     },
